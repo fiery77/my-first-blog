6dfeced396c3650a568c813197fde6ffe977dded
gdunn <gdunn@essex.ac.uk>
2018-06-06 16:26:28 +0100

    Latest round of HTML mods

--- blog/views.py
+++ blog/views.py
@@ -4,4 +4,5 @@ from .models import Post
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    #posts = Post.objects.order_by('created_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
